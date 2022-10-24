1a8e85c54d419d2d70d828d6891102e31b7ca877
Kit Gorn <nik_dezel@mail.ru>
2022-10-24 21:32:38 +0500

    Cleaned up tabs. Changed table structure in sql query.

--- ShapeLibrary/SQL/SelectProductCategory.sql
+++ ShapeLibrary/SQL/SelectProductCategory.sql
@@ -1,7 +1,6 @@
-﻿-- Запрос по заданию №2 в конце документа
-
-DROP TABLE IF EXISTS [Product];
+﻿DROP TABLE IF EXISTS [Product];
 DROP TABLE IF EXISTS [Category];
+DROP TABLE IF EXISTS [KEYS_Product_Category];
 
 -- Создаем таблицы
 
@@ -15,28 +14,53 @@ CREATE TABLE [Product]
 (  
 	[Id] int IDENTITY(1,1) PRIMARY KEY,  
 	[Name] nvarchar (50),
+);
+
+CREATE TABLE [KEYS_Product_Category]
+(
+	[ProductId] int,
 	[CategoryId] int, 
 	FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id])
+	ON DELETE CASCADE
+	ON UPDATE CASCADE,
+	FOREIGN KEY ([ProductId]) REFERENCES [Product] ([Id])
 	ON DELETE CASCADE
 	ON UPDATE CASCADE
 );
 
 -- Заполняем данными
 
-INSERT INTO [Category] ([Name]) VALUES ('Овощи');
 INSERT INTO [Category] ([Name]) VALUES ('Фрукты');
 INSERT INTO [Category] ([Name]) VALUES ('Сладости');
+INSERT INTO [Category] ([Name]) VALUES ('Выпечка');
+INSERT INTO [Category] ([Name]) VALUES ('Жидкости');
 
 
-INSERT INTO [Product] ([Name], [CategoryId]) VALUES ('Огурец', 1);
-INSERT INTO [Product] ([Name], [CategoryId]) VALUES ('Помидор', 1);
-INSERT INTO [Product] ([Name], [CategoryId]) VALUES ('Яблоко', 2);
-INSERT INTO [Product] ([Name], [CategoryId]) VALUES ('Груша', 2);
-INSERT INTO [Product] ([Name], [CategoryId]) VALUES ('Шоколад', 3);
-INSERT INTO [Product] ([Name], [CategoryId]) VALUES ('Конфеты', 3);
+INSERT INTO [Product] ([Name]) VALUES ('Яблоко');
+INSERT INTO [Product] ([Name]) VALUES ('Груша');
+INSERT INTO [Product] ([Name]) VALUES ('Шоколад');
+INSERT INTO [Product] ([Name]) VALUES ('Конфеты');
 INSERT INTO [Product] ([Name]) VALUES ('Кирпич');
+INSERT INTO [Product] ([Name]) VALUES ('Финики');
+INSERT INTO [Product] ([Name]) VALUES ('Яблоко в шоколаде');
+INSERT INTO [Product] ([Name]) VALUES ('Пирожок с мясом');
+INSERT INTO [Product] ([Name]) VALUES ('Пирожок с малиной');
+
+INSERT INTO [KEYS_Product_Category] ([ProductId], [CategoryId]) VALUES (1, 1);
+INSERT INTO [KEYS_Product_Category] ([ProductId], [CategoryId]) VALUES (2, 1);
+INSERT INTO [KEYS_Product_Category] ([ProductId], [CategoryId]) VALUES (3, 2);
+INSERT INTO [KEYS_Product_Category] ([ProductId], [CategoryId]) VALUES (4, 2);
+INSERT INTO [KEYS_Product_Category] ([ProductId], [CategoryId]) VALUES (6, 1);
+INSERT INTO [KEYS_Product_Category] ([ProductId], [CategoryId]) VALUES (6, 2);
+INSERT INTO [KEYS_Product_Category] ([ProductId], [CategoryId]) VALUES (7, 1);
+INSERT INTO [KEYS_Product_Category] ([ProductId], [CategoryId]) VALUES (7, 2);
+INSERT INTO [KEYS_Product_Category] ([ProductId], [CategoryId]) VALUES (8, 3);
+INSERT INTO [KEYS_Product_Category] ([ProductId], [CategoryId]) VALUES (9, 2);
+INSERT INTO [KEYS_Product_Category] ([ProductId], [CategoryId]) VALUES (9, 3);
 
 -- Собственно, выводим сам запрос
 
 SELECT [Product].[Name] AS 'Имя продукта', [Category].[Name] AS 'Имя категории' 
-	FROM [Product] LEFT JOIN [Category] ON [Product].[CategoryId] = [Category].[Id]
+	FROM [Product] 
+	LEFT JOIN [KEYS_Product_Category] ON [KEYS_Product_Category].[ProductId] = [Product].[Id]
+	LEFT JOIN [Category] ON [KEYS_Product_Category].[CategoryId] = [Category].[Id]
--- ShapeLibrary/ShapeMath.cs
+++ ShapeLibrary/ShapeMath.cs
@@ -3,75 +3,75 @@ using System;
 
 namespace ShapeLib
 {
-    public static class ShapeMath
-    {
-        public static float GetArea(IShape shape)
-        {
-            return shape.GetArea();
-        }
-
-        public static float GetArea(dynamic shape)
-        {
-			if (shape is IShape s)
-            {
-                return s.GetArea();
-            }
-            else
-            {
-                throw new InvalidCastException("Variable \"shape\" not has type \"IShape\"!");
-            }
+	public static class ShapeMath
+	{
+		public static float GetArea(IShape shape)
+		{
+			return shape.GetArea();
 		}
 
-        public static float GetCircleArea(float r)
+		public static float GetArea(dynamic shape)
 		{
-            ValidateRadius(r);
+			if (shape is IShape s)
+			{
+				return s.GetArea();
+			}
+			else
+			{
+				throw new InvalidCastException("Variable \"shape\" not has type \"IShape\"!");
+			}
+		}
+
+		public static float GetCircleArea(float r)
+		{
+			ValidateRadius(r);
 			return (float)Math.PI * r * r;
 		}
 
-        public static float GetTriangleArea(float a, float b, float c)
+		public static float GetTriangleArea(float a, float b, float c)
 		{
 			ValidateSidesOfTriangle(a, b, c);
 
 			var hP = (a + b + c) / 2f;
-            return (float)Math.Sqrt(hP * (hP - a) * (hP - b) * (hP - c));
-        }
+			return (float)Math.Sqrt(hP * (hP - a) * (hP - b) * (hP - c));
+		}
 
-        public static bool IsRightTriangle(float a, float b, float c)
-        {
-            ValidateSidesOfTriangle(a, b, c);
+		public static bool IsRightTriangle(float a, float b, float c)
+		{
+			ValidateSidesOfTriangle(a, b, c);
 
-            var a2 = a * a;
-            var b2 = b * b;
-            var c2 = c * c;
+			var a2 = a * a;
+			var b2 = b * b;
+			var c2 = c * c;
 			
-            return Math.Abs(b2 + a2 - c2) <= float.Epsilon 
-                || Math.Abs(b2 + c2 - a2) <= float.Epsilon 
-                || Math.Abs(a2 + c2 - b2) <= float.Epsilon;
-        }
+			return Math.Abs(b2 + a2 - c2) <= float.Epsilon 
+				|| Math.Abs(b2 + c2 - a2) <= float.Epsilon 
+				|| Math.Abs(a2 + c2 - b2) <= float.Epsilon;
+		}
 
-        public static bool IsRightTriangle(Triangle triangle) 
-        {
-            return triangle.IsRightTriangle();
-        }
+		public static bool IsRightTriangle(Triangle triangle) 
+		{
+			return triangle.IsRightTriangle();
+		}
 
-        internal static void ValidateSidesOfTriangle(float a, float b, float c)
-        {
+		internal static void ValidateSidesOfTriangle(float a, float b, float c)
+		{
 			if (a <= 0 || b <= 0 || c <= 0)
 			{
 				throw new ArgumentException("Sides of triangle must be greater than zero!");
 			}
-            if (a + b <= c || b + c <= a || c + a <= b)
+			if (a + b <= c || b + c <= a || c + a <= b)
 			{
 				throw new ArgumentException($"A triangle with such sides cannot exist! Sides {a}, {b}, {c}");
 			}
 		}
 
-        internal static void ValidateRadius(float r)
-        {
+		internal static void ValidateRadius(float r)
+		{
 			if (r <= 0)
 			{
 				throw new ArgumentException("Radius must be greater than zero!");
 			}
 		}
-    }
+	}
 }
